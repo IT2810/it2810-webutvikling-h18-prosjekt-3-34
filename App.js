8f14207f322d16df13225eed461b73a9adaf7ad9
Erikstelarsen <Erikstelarsen@outlook.com>
2018-09-26 13:51:53 +0200

    Tester et veldig basic design av noen rader #1

--- App.js
+++ App.js
@@ -1,21 +1,68 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react'
+import { Text, View, TouchableOpacity, StyleSheet } from 'react-native'
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
+export default class List extends Component {
+   state = {
+      rader: [
+         {
+            id: 0,
+            name: 'Rad 1',
+         },
+         {
+            id: 1,
+            name: 'Rad 2',
+         },
+         {
+            id: 2,
+            name: 'Rad 3',
+         },
+         {
+            id: 3,
+            name: 'Rad 4',
+         }
+      ]
+   }
+   alertItemName = (item) => {
+      alert("asdasdasda")
+   }
+   render() {
+      return (
+         <View style={styles.mainContainer}>
+            {
+               this.state.rader.map((item) => (
+
+                  <TouchableOpacity
+                     key = {item.id}
+                     style = {styles.rad}
+                     onPress = {() => this.alertItemName(item)}>
+
+                     <Text key= {item.id} style = {styles.text}>
+                        {item.name}
+                     </Text>
+                  </TouchableOpacity>
+
+               ))
+            }
+         </View>
+      )
+   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const styles = StyleSheet.create ({
+  mainContainer: {
+    marginTop: 15
   },
-});
+  kolonne: {
+    padding: 10,
+  },
+   rad: {
+      display: 'flex',
+      padding: 10,
+      marginTop: 5,
+      backgroundColor: '#d9f9b1',
+      alignItems: 'center',
+   },
+   text: {
+      color: '#4f603c'
+   }
+})
