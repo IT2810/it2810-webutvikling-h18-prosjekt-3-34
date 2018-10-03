84aacf84c6e5360185c7c68852204346c46a25ac
Thusan Arul <piruthusan@gmail.com>
2018-10-03 14:31:45 +0200

    style den litt. er egentlig ferdig nå

--- components/datecomponent.js
+++ components/datecomponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Text, View, TouchableOpacity, StyleSheet, Button, Vibration} from 'react-native'
+import { Text, View, TouchableOpacity, StyleSheet, Button, Vibration, TouchableWithoutFeedback} from 'react-native'
 import styles from "../stylesheets/datecomponent.style.js"
 
 export default class DateComponent extends Component {
@@ -8,21 +8,59 @@ export default class DateComponent extends Component {
   }
 
   render(){
+    let viewDate = this.props.viewDate;
+    let currentDate = new Date();
     var dateFormat = require("dateformat");
-    return(
-      <View>
-        <TouchableOpacity style = {styles.prevDay} onPress={this.props.handlePrevDayClick}>
-          <Text>-1</Text>
-        </TouchableOpacity>
+    switch(viewDate.getDay()){
+      case currentDate.getDay():
+        return(
+          <View style={styles.dateBox}>
+            <TouchableOpacity style = {styles.prevDay} onPress={this.props.handlePrevDayClick}>
+              <Text>-1</Text>
+            </TouchableOpacity>
 
-        <Text>{dateFormat(this.props.viewDate, "dddd, mmmm dS")}</Text>
+            <Text style = {styles.dateText}>{dateFormat(viewDate, "dddd, mmmm dS")}</Text>
 
-        <TouchableOpacity style = {styles.prevDay} onPress={this.props.handleNextDayClick}>
-          <Text>+1</Text>
-        </TouchableOpacity>
-      </View>
-    );
+            <TouchableOpacity style = {styles.nextDay} onPress={this.props.handleNextDayClick}>
+              <Text>+1</Text>
+            </TouchableOpacity>
+          </View>
+        );
+      case currentDate.getDay()+1:
+        return(
+          <View style={styles.dateBox}>
+            <TouchableOpacity style = {styles.prevDay} onPress={this.props.handlePrevDayClick}>
+              <Text>-1</Text>
+            </TouchableOpacity>
+
+            <Text style = {styles.dateText}>{dateFormat(viewDate, "dddd, mmmm dS")}</Text>
+
+            <TouchableWithoutFeedback>
+              <View style = {styles.nextDayClicked}>
+                <Text>+1</Text>
+              </View>
+            </TouchableWithoutFeedback>
+          </View>
+        );
+      case currentDate.getDay()-1:
+        return(
+          <View style={styles.dateBox}>
+            <TouchableWithoutFeedback>
+              <View style = {styles.prevDayClicked}>
+                <Text>-1</Text>
+              </View>
+            </TouchableWithoutFeedback>
+
+            <Text style = {styles.dateText}>{dateFormat(viewDate, "dddd, mmmm dS")}</Text>
+
+            <TouchableOpacity style = {styles.nextDay} onPress={this.props.handleNextDayClick}>
+              <Text>+1</Text>
+            </TouchableOpacity>
+          </View>
+        );
+
+    }
   }
 }
 
-Expo.registerRootComponent(DateComponent);
+//Expo.registerRootComponent(DateComponent);
--- stylesheets/datecomponent.style.js
+++ stylesheets/datecomponent.style.js
@@ -1,14 +1,54 @@
-import {StyleSheet} from "react-native"
+import {StyleSheet} from "react-native";
 
 export default StyleSheet.create({
+  dateBox: {
+    backgroundColor: "#dedede",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+
   prevDay: {
    justifyContent: "center",
    alignItems: "center",
-   backgroundColor: "#4f4f4f",
+   backgroundColor: "#00c652",
    height: 35,
-   width: 110,
+   width: 70,
    borderRadius: 5,
    borderWidth: 1,
-   marginTop: 15,
-  }
+ },
+
+ nextDay: {
+  justifyContent: "center",
+  alignItems: "center",
+  backgroundColor: "#00c652",
+  height: 35,
+  width: 70,
+  borderRadius: 5,
+  borderWidth: 1,
+},
+dateText: {
+  fontSize: 16,
+  textAlignVertical: "center",
+},
+prevDayClicked: {
+ justifyContent: "center",
+ alignItems: "center",
+ backgroundColor: "gray",
+ height: 35,
+ width: 70,
+ borderRadius: 5,
+ borderWidth: 1,
+},
+
+nextDayClicked: {
+justifyContent: "center",
+alignItems: "center",
+backgroundColor: "gray",
+height: 35,
+width: 70,
+borderRadius: 5,
+borderWidth: 1,
+},
+
 })
