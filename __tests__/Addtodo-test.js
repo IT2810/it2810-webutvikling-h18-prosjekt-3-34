072e8e5d7e6b500506a0283cee05a4be709cb6f9
Magnus Gaustad Eriksson <magnuge@stud.ntnu.no>
2018-10-08 13:46:38 +0200

    lagt til test for metoder som endrer state til addTodo

--- __tests__/Addtodo-test.js
+++ __tests__/Addtodo-test.js
@@ -20,3 +20,40 @@ const result = shallow.getRenderOutput();
 test("Renders correct type", () => {
   expect(result.type).toBe(Modal);
 });
+
+it("ToggleTodoInput should change showTodoInputField", () => {
+  let addTodoComponent = renderer
+    .create(<Addtodo setType={() => {}} />) // Not interested to test setType function of App.
+    .getInstance();
+  expect(addTodoComponent.state.showTodoInputField).toBe(false);
+  addTodoComponent.toggleTodoInput();
+  expect(addTodoComponent.state.showTodoInputField).toBe(true);
+  addTodoComponent.toggleTodoInput();
+  expect(addTodoComponent.state.showTodoInputField).toBe(false);
+});
+
+it("ToggleStepInput should change showStepInputField", () => {
+  let addTodoComponent = renderer
+    .create(<Addtodo setType={() => {}} />) // Not interested to test setType function of App.
+    .getInstance();
+  expect(addTodoComponent.state.showStepInputField).toBe(false);
+  addTodoComponent.toggleStepInput();
+  expect(addTodoComponent.state.showStepInputField).toBe(true);
+  addTodoComponent.toggleStepInput();
+  expect(addTodoComponent.state.showStepInputField).toBe(false);
+});
+
+it("CloseModal should set all States to false", () => {
+  let addTodoComponent = renderer
+    .create(<Addtodo toggleModal={() => {}} setType={() => {}} />) // Not interested to test toggle modal function of App.
+    .getInstance();
+  expect(addTodoComponent.state.showStepInputField).toBe(false);
+  expect(addTodoComponent.state.showTodoInputField).toBe(false);
+  addTodoComponent.toggleTodoInput();
+  addTodoComponent.toggleStepInput();
+  expect(addTodoComponent.state.showStepInputField).toBe(true);
+  expect(addTodoComponent.state.showTodoInputField).toBe(true);
+  addTodoComponent.closeModal();
+  expect(addTodoComponent.state.showStepInputField).toBe(false);
+  expect(addTodoComponent.state.showTodoInputField).toBe(false);
+});
