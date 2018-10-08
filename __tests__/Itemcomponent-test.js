84e70b3dbeebf2cfea1cfba94dfd00fab9917a72
Magnus Gaustad Eriksson <magnuge@stud.ntnu.no>
2018-10-08 14:00:49 +0200

    lagt til funksjonstest for itemcomponent

--- __tests__/Itemcomponent-test.js
+++ __tests__/Itemcomponent-test.js
@@ -3,7 +3,17 @@ import Itemcomponent from ".././components/itemcomponent";
 
 import renderer from "react-test-renderer";
 
+//Snapshot test
 test("renders correctly", () => {
   const tree = renderer.create(<Itemcomponent />).toJSON();
   expect(tree).toMatchSnapshot();
+});
+
+//Test statechanging methods
+it("hasDoneClick should change background color to green", () => {
+  let itemComponent = renderer.create(<Itemcomponent />).getInstance();
+
+  expect(itemComponent.state.backgroundColor).toBe("#bdbdbd");
+  itemComponent.handleDoneClick();
+  expect(itemComponent.state.backgroundColor).toBe("green");
 });
