e3fd602fea29db303a37864d0ed57ca23b8f2ac4
Magnus Gaustad Eriksson <magnuge@stud.ntnu.no>
2018-10-17 18:40:08 +0200

    added viewdate to Datecomponent test so it renders correctly

--- __tests__/Datecomponent-test.js
+++ __tests__/Datecomponent-test.js
@@ -5,6 +5,8 @@ import renderer from "react-test-renderer";
 
 //Snapshot test
 test("renders correctly", () => {
-  const tree = renderer.create(<DateComponent />).toJSON();
+  //Sets the viewdate to today.
+  const viewDate = new Date();
+  const tree = renderer.create(<DateComponent viewDate={viewDate} />).toJSON();
   expect(tree).toMatchSnapshot();
 });
