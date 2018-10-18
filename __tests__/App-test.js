ea3897685affbcece48e8fd0c819b3539e6a4576
Magnus Gaustad Eriksson <magnuge@stud.ntnu.no>
2018-10-18 18:47:40 +0200

    Lagt til mock objekt for async storage

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -1,14 +1,33 @@
 import React from "react";
 import ShallowRenderer from "react-test-renderer/shallow";
-import renderer from "react-test-renderer";
-import App from "../App";
-import MockStorage from "./mockStorage";
-import AsyncStorage from "react-native";
-let dateNowSpy;
+import { AsyncStorage as storage } from "react-native";
 
-const storageCache = {};
-const asyncStorage = new MockStorage(storageCache);
-jest.setMock("AsyncStorage", asyncStorage);
+import App from "../App";
+import MockAsyncStorage from "mock-async-storage";
+import renderer from "react-test-renderer";
+
+const mockAsync = () => {
+  const mockImpl = new MockAsyncStorage();
+  jest.mock("AsyncStorage", () => mockImpl);
+};
+
+const releaseMockAsync = () => jest.unmock("AsyncStorage");
+
+beforeAll(() => {
+  console.log("Start");
+  // Set Date object to a deterministic value
+
+  // Setup for AsyncStorage mock
+  mockAsync();
+});
+
+afterAll(() => {
+  //Release Date
+
+  //Release mockAsync
+  releaseMockAsync();
+  console.log("end");
+});
 
 //Snapshot test
 const shallow = new ShallowRenderer();
@@ -57,6 +76,8 @@ it("handlePrevDayClick should set a new date", () => {
   expect(appComponent.state.viewDate.getDate()).toBe(yesterDay);
 });
 
-it("Asynchstorage is empty first time", () => {
-  expect(appComponent.AsynchStorage).toBe(null);
+it("Mock async storage working", async () => {
+  await storage.setItem("steps", "15000");
+  const value = await storage.getItem("steps");
+  expect(value).toBe("15000");
 });
